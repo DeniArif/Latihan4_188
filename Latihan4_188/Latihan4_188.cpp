5dcd59df2fef5cc0070269fcee56073c6d246d37
DeniArif <deniarifwibowo82@gmail.com>
2023-03-20 14:53:07 +0700

    Comit !

--- Latihan4_188/Latihan4_188.cpp
+++ Latihan4_188/Latihan4_188.cpp
@@ -1,1 +1,25 @@
-#
+#include <iostream>
+using namespace std;
+
+float luasPersegi(float p, float l) {
+	return p * l;
+}
+float LuasLingkaran(float r) {
+	return 3.14 * r * r;
+}
+float LuasSegitiga(float a, float t) {
+	return 0.5 * a * t;
+}
+
+int main()
+{
+	float panjang, lebar, jejari, alas, tinggi;
+	int pilihan;
+
+	do {
+		cout << "\n\n===================" << endl;
+		cout << "=======MENU ==========" << endl;
+		cout << "====================" << endl;
+		cout << "1. Luas Persegi " << endl;
+	}
+}
